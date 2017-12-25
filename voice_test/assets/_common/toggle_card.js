7e77475763bd3209a4a715719460335480ba541a
earl <604458675@qq.com>
2017-12-25 15:38:57 +0800

    动效模板基本，翻卡模板基本完成

--- voice_test/assets/_common/toggle_card.js
+++ voice_test/assets/_common/toggle_card.js
@@ -17,8 +17,10 @@ cc.Class({
         var self = this;
         // cursor pointer
         self.node.on(cc.Node.EventType.MOUSE_ENTER, function(event) {
-            cc._canvas.style.cursor = 'pointer';
-            self.node.style.cursor = 'pointer';
+            var timer = setInterval(function() {
+                cc._canvas.style.cursor = 'pointer';
+                clearInterval(timer);
+            }, 100);
         });
         self.node.on(cc.Node.EventType.MOUSE_LEAVE, function(event) {
             cc._canvas.style.cursor = 'default';
@@ -29,9 +31,9 @@ cc.Class({
             var nodeName = self.node.name;
             if (!nodeName) return;
             if (nodeName == "nextPageBtn") {
-                self.nextPageBtn("");
+                self.nextPageBtn();
             } else if (nodeName == "upPageBtn") {
-                self.upPageBtn("");
+                self.upPageBtn();
             }
         });
     },
@@ -44,10 +46,9 @@ cc.Class({
         var self = this;
         var upPageBtnNode = cc.find('Canvas/student/upPageBtn');
         var nextPageBtnNode = cc.find('Canvas/student/nextPageBtn');
-        if (!upPageBtnNode || !nextPageBtnNode) return;
-        upPageBtnNode.opacity = 0;
-        upPageBtnNode.zIndex = 0;
-        if (self.cards && self.cards.length < 2 || !self.isTeacher) {
+        if (!self.isTeacher) {
+            nextPageBtnNode.opacity = 0;
+            nextPageBtnNode.zIndex = 0;
             upPageBtnNode.opacity = 0;
             upPageBtnNode.zIndex = 0;
         }
@@ -62,26 +63,11 @@ cc.Class({
             if (!nextCard) return;
             var canActive = null;
             var currentNode = cc.find('Canvas/student/cardList/' + activeCard);
-
-            currentNode.zIndex = 2;
-
             currentNode.opacity = 0;
-
             var nextNode = cc.find('Canvas/student/cardList/' + nextCard);
             nextNode.opacity = 255;
             nextNode.zIndex = 1;
             if (window.nova && window.nova.toggleCard && self.isTeacher) window.nova.toggleCard("nextPageBtn", false);
-            disB();
-
-            function disB() {
-                canActive = self.canNextActiveCard();
-                if (!canActive) {
-                    self.node.opacity = 0;
-                    self.node.zIndex = 0;
-                } else if (self.isTeacher) {
-                    cc.find('Canvas/student/upPageBtn').opacity = 255;
-                }
-            }
         }
     },
 
@@ -95,26 +81,11 @@ cc.Class({
 
             var canActive = null;
             var currentNode = cc.find('Canvas/student/cardList/' + activeCard);
-
-            currentNode.zIndex = 2;
-
             currentNode.opacity = 0;
-
             var upNode = cc.find('Canvas/student/cardList/' + upCard);
             upNode.opacity = 255;
             upNode.zIndex = 1;
             if (window.nova && window.nova.toggleCard && self.isTeacher) window.nova.toggleCard("upPageBtn", false);
-            disB();
-
-            function disB() {
-                canActive = self.canUpActiveCard();
-                if (!canActive) {
-                    self.node.opacity = 0;
-                    self.node.zIndex = 0;
-                } else if (self.isTeacher) {
-                    cc.find('Canvas/student/nextPageBtn').opacity = 255;
-                }
-            }
         }
     },
 
@@ -165,12 +136,12 @@ cc.Class({
 
     getNextCard: function(activeCard, cards) {
         for (var i = 0; i < cards.length; i++) {
-            if (cards[i] == activeCard) return i == cards.length - 1 ? null : cards[i + 1];
+            if (cards[i] == activeCard) return i == cards.length - 1 ? cards[0] : cards[i + 1];
         }
     },
     getUpCard: function(activeCard, cards) {
         for (var i = 0; i < cards.length; i++) {
-            if (cards[i] == activeCard) return i == 0 ? null : cards[i - 1];
+            if (cards[i] == activeCard) return i == 0 ? cards[cards.length - 1] : cards[i - 1];
         }
     }
 });
