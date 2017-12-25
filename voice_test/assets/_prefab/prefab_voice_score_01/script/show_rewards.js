4e5a2ac6cdf777a56bf3201e8a09840f84280a48
earl <604458675@qq.com>
2017-12-25 11:54:40 +0800

    小手更改

--- voice_test/assets/_prefab/prefab_voice_score_01/script/show_rewards.js
+++ voice_test/assets/_prefab/prefab_voice_score_01/script/show_rewards.js
@@ -19,8 +19,10 @@ cc.Class({
 
         // cursor pointer
         self.node.on(cc.Node.EventType.MOUSE_ENTER, function(event) {
-            cc._canvas.style.cursor = 'pointer';
-            self.node.style.cursor = 'pointer';
+            var timer = setInterval(function() {
+                cc._canvas.style.cursor = 'pointer';
+                clearInterval(timer);
+            }, 100);
         });
         self.node.on(cc.Node.EventType.MOUSE_LEAVE, function(event) {
             cc._canvas.style.cursor = 'default';
