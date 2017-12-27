051b9935a17ae299ab107dfdeaf92179f02fef40
earl <604458675@qq.com>
2017-12-27 10:25:27 +0800

    12.27.02 添加自定义事件

--- OneClassStatistics/assets/_common/_analytics.js
+++ OneClassStatistics/assets/_common/_analytics.js
@@ -13,8 +13,13 @@ cc.Class({
         cocosAnalytics.CAAccount.loginStart();
         cocosAnalytics.CAAccount.loginSuccess({ 'userID': '101' });
         cocosAnalytics.onPause(true);
-        cocosAnalytics.enableDebug(true);
         self.gameStart(sceneName); // 关卡开始
+
+        // 自定义事件
+        // 参数：事件ID（必填）, 不得超过30个字符
+        cocosAnalytics.CAEvent.onEvent({
+            eventName: "打开游戏"
+        })
     },
 
     onDisable: function() {
