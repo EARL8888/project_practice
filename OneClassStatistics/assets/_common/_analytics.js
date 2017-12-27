254532b7604deb25961d3381a062da7b14731380
earl <604458675@qq.com>
2017-12-27 10:49:20 +0800

    用户信息，固定封板

--- OneClassStatistics/assets/_common/_analytics.js
+++ OneClassStatistics/assets/_common/_analytics.js
@@ -11,7 +11,7 @@ cc.Class({
         var self = this;
         var sceneName = cc.director.getScene().name;
         cocosAnalytics.CAAccount.loginStart();
-        cocosAnalytics.CAAccount.loginSuccess({ 'userID': '101' });
+        cocosAnalytics.CAAccount.loginSuccess({ 'userID': '101'});
         cocosAnalytics.onPause(true);
         self.gameStart(sceneName); // 关卡开始
 
@@ -19,7 +19,9 @@ cc.Class({
         // 参数：事件ID（必填）, 不得超过30个字符
         cocosAnalytics.CAEvent.onEvent({
             eventName: "打开游戏"
-        })
+        });
+        //开启/关闭本地日志的输出
+        cocosAnalytics.enableDebug(true);
     },
 
     onDisable: function() {
