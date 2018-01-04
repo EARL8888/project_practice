2a37bdfbc6222d7245e87df6eba634f6d4441962
earl <604458675@qq.com>
2018-01-04 20:58:01 +0800

    解决重进动画不同步问题

--- voice_test/assets/_prefab/prefab_voice_score_01/script/voice_anim.js
+++ voice_test/assets/_prefab/prefab_voice_score_01/script/voice_anim.js
@@ -11,6 +11,7 @@ cc.Class({
         var self = this;
         self.triggleClick();
         self.isTeacher = self.isTeacherMothed();
+        self.studentShowAnim();
     },
 
     isTeacherMothed: function() {
@@ -29,7 +30,7 @@ cc.Class({
 
         // click
         self.node.on(cc.Node.EventType.TOUCH_END, function(event) {
-            self.voiceAnimShowOrStop();
+            self.voiceAnimShowOrStop_01();
             // send msg
             if (window.nova && window.nova.voiceAnimShowOrStop && self.isTeacher) window.nova.voiceAnimShowOrStop(self.node.name);
         });
@@ -37,23 +38,68 @@ cc.Class({
 
     voiceAnimShowOrStop: function() {
         var self = this;
-        var voice_anim = window.localStorage.getItem('voice_anim');
-        if (voice_anim == 1) {
-            self.getComponent(cc.Animation).stop("anim_01");
-            window.localStorage.setItem('voice_anim', null);
-        } else {
-            self.getComponent(cc.Animation).play("anim_01");
-            window.localStorage.setItem('voice_anim', 1);
-        }
+        window.nova.get('voice_anim', function(msg) {
+            var voice_anim = msg;
+            console.log("voice_anim:" + voice_anim);
+            if (voice_anim == 1) {
+                self.getComponent(cc.Animation).stop("anim_01");
+            } else {
+                self.getComponent(cc.Animation).play("anim_01");
+            }
+        });
+    },
+
+    voiceAnimShowOrStop_01: function() {
+        var self = this;
+        window.nova.get('voice_anim', function(msg) {
+            var voice_anim = msg;
+            console.log("voice_anim_01:" + voice_anim);
+            if (voice_anim == 1) {
+                self.getComponent(cc.Animation).stop("anim_01");
+                if (self.isTeacher) {
+                    self.setState('voice_anim', '');
+                }
+            } else {
+                self.getComponent(cc.Animation).play("anim_01");
+                if (self.isTeacher) {
+                    self.setState('voice_anim', 1);
+                }
+            }
+        });
+    },
+
+    setState: function(name, tag) {
+        setTimeout(function() {
+            window.nova.set(name, tag);
+        }, 500);
+    },
+
+    studentShowAnim: function() {
+        var self = this;
+        var voice_anim = window.nova.get('voice_anim', function(msg) {
+            var voice_anim = msg;
+            console.log("studentShowAnim:" + voice_anim);
+            if (voice_anim == 1) {
+                self.getComponent(cc.Animation).play("anim_01");
+            } else {
+                self.getComponent(cc.Animation).stop("anim_01");
+            }
+        });
     },
 
     voiceStopAnim: function() {
         var self = this;
         self.getComponent(cc.Animation).stop("anim_01");
-        window.localStorage.setItem('voice_anim', null);
+        if (self.isTeacher) {
+            self.setState('voice_anim', '');
+        }
     },
 
     onDisable() {
-        window.localStorage.setItem('voice_anim', null);
+        var self = this;
+        if (self.isTeacher) {
+            self.setState('voice_anim', '');
+            console.log("进来onDisable");
+        }
     },
 });
